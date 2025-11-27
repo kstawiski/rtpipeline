746c4f1af87125750ad037b34807676144d1534b
Konrad Stawiski <konrad@konsta.com.pl>
2025-11-27 12:56:56 +0100

    Update public RTpipeline software.

--- run_rtpipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# RTpipeline Execution Script
-# Platform: Docker
-set -e
-
-echo "Starting RTpipeline in Docker..."
-docker run -it --rm --gpus all -v "/projekty/Odbytnice/DICOM":/data/input:ro -v "/projekty/Odbytnice/DICOM_rtpipeline":/data/output:rw kstawiski/rtpipeline:latest snakemake --cores all --use-conda --configfile /data/output/config.yaml "$@"
